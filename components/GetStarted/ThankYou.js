7719f0cb4a1cdd807e3302a97e6bfce0a894ad49
Remely <remely@createdbyblack.com>
2023-01-19 17:24:23 +0800

    get started thanks form

--- components/GetStarted/ThankYou.js
+++ components/GetStarted/ThankYou.js
@@ -5,13 +5,15 @@ import styles from '../../styles/pages/_Getstarted.module.scss';
 const ThankYou = () => {
   return (
     <>
-      <h1>Thank You</h1>
-      <p>
-        Our team will review your inquiry and one<br></br>
-        of our light specialists will contact you<br></br>
-        with next steps.
-      </p>
-      <button type="button">BACK TO HOME</button>
+      <div className='thank_you'> 
+        <h1>Thank You</h1>
+        <p>
+          Our team will review your inquiry and one
+          of our light specialists will contact you
+          with next steps.
+        </p>
+        <button type="button">BACK TO HOME</button>
+      </div>
     </>
   );
 };
